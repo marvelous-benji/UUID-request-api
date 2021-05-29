593e92b63e394f5a96f30b345d79006d11dd4e55
marvelous-benji <61952411+marvelous-benji@users.noreply.github.com>
2021-05-29 06:56:29 +0100

    Added comments

--- RequestUuid/tests.py
+++ RequestUuid/tests.py
@@ -12,6 +12,6 @@ class TestUUIDEndpoint(APITestCase):
     def test_get_request(self):
         url = reverse('get_uuids')
         response = self.client.get(url, format='json')
-        self.assertTrue(response.status_code, status.HTTP_200_OK)
-        self.assertIsNotNone(UUIDGenerator.objects.first())
-        self.assertEqual(UUIDGenerator.objects.count(),1)
+        self.assertTrue(response.status_code, status.HTTP_200_OK) #tests that the request was successfull
+        self.assertIsNotNone(UUIDGenerator.objects.first()) #tests to see that the dictionary is initialized at the start of the request
+        self.assertEqual(UUIDGenerator.objects.count(),1) #tests to see that a timstamp-uuid data is created
